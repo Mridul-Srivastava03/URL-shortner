3823be85f7a5bc3a3b056c6663937df5e6d7aa55
Kumar Mridul <kummridul03@gmail.com>
2026-04-25 20:37:06 +0530

    initial commit with database config

---
+++ src/test/java/com/projects/self/system_design/url_shortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.self.system_design.url_shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
