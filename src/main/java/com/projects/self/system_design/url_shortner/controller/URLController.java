79c97725b1e6538e91c1f16624ad74bab48add4e
Kumar Mridul <kummridul03@gmail.com>
2026-04-26 23:36:02 +0530

    Debugged the changes to make it proper working

--- src/main/java/com/projects/self/system_design/url_shortner/controller/URLController.java
+++ src/main/java/com/projects/self/system_design/url_shortner/controller/URLController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
-@RestController("/api/url")
+@RestController
+@RequestMapping(("/api/url"))
 public class URLController {
 
     private final URLService service;
