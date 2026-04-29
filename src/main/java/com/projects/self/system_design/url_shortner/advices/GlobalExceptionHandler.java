cfd863a058708623998e5015540c52b18a0842c6
Kumar Mridul <kummridul03@gmail.com>
2026-04-29 05:35:15 +0530

    Added GlobalExceptionHandler to handle exception properly

---
+++ src/main/java/com/projects/self/system_design/url_shortner/advices/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.projects.self.system_design.url_shortner.advices;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleInputValidationException (MethodArgumentNotValidException exception) {
+        List<String> errors = exception
+                .getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(exception.getMessage())
+                .subErrors(errors)
+                .build();
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+}
